7687e42c82c0ed68242c6dd826911a83eba9f446
MantisClone <david.huntmateo@request.network>
2024-05-30 16:19:25 -0400

    Revert "Revert "fix: changed the style of the dropdown"" (#30)

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -26,24 +26,28 @@ const Navbar = () => {
 
   const supportLinks = [
     {
-      name: "Discord",
-      href: "https://discord.com/channels/468974345222619136/1103420140181274645",
-    },
-    {
       name: "Github Discussions",
       href: "https://github.com/orgs/RequestNetwork/discussions",
+    },
+    {
+      name: "Discord",
+      href: "https://discord.com/channels/468974345222619136/1103420140181274645",
     },
   ];
 
   return (
     <nav className="h-full flex items-center p-[20px] gap-[60px] bg-white shadow-small mb-[80px]">
-      <Link href="/">
+      <a
+        target="_blank"
+        rel="noreferrer noopener"
+        href="https://request.network/"
+      >
         <img
           src="assets/logo.svg"
           alt="Request Network Logo"
           className="w-[120px]"
         />
-      </Link>
+      </a>
       <ul className="h-full flex gap-[60px]">
         {links.map((link, index) => (
           <li className={`h-full relative text-black`} key={index}>
@@ -51,7 +55,7 @@ const Navbar = () => {
             <div
               className={`${
                 router.pathname === link.href &&
-                "h-[4px] bg-[#0BB489] w-full absolute bottom-[-28px]"
+                "h-[4px] bg-green w-full absolute bottom-[-28px]"
               }`}
             ></div>
           </li>
@@ -66,15 +70,15 @@ const Navbar = () => {
             target="_blank"
             rel="noreferrer noopener"
             href="https://docs.request.network/building-blocks/templates"
-            className="flex items-center gap-[5px] bg-transparent text-[#0BB489] font-medium text-[16px]"
+            className="flex items-center gap-[5px] bg-transparent text-green font-medium text-[16px]"
           >
             Integrate in your app
             <ArrowUpRight />
           </a>
           <div
             className={`${
-              isDocsHovered ? "h-[2px]" : "h-[0px]"
-            } w-100 bg-[#0BB489]`}
+              isDocsHovered ? "h-[1.5px]" : "h-[0px]"
+            } w-100 bg-green`}
           ></div>
         </div>
         <Dropdown title="Need help?" items={supportLinks} />
