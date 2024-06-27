e77d03073fe9dc9e7b0a52f00f43c45083e965ba
MantisClone <david.huntmateo@request.network>
2024-06-27 18:37:43 -0400

    fix: getSubgraphClient with ternary. Add all supported subgraphs.

--- utils/initializeRN.ts
+++ utils/initializeRN.ts
@@ -16,13 +16,53 @@ export const initializeRequestNetwork = (setter: any, walletClient: any) => {
       },
       paymentOptions: {
         getSubgraphClient: (chain: string) => {
-          const envVarName = `NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_${chain.toUpperCase()}`;
-          console.log(`Accessing environment variable: ${envVarName}`);
-          const paymentsSubgraphUrl = process.env[envVarName]!;
-          console.log(`Value: ${paymentsSubgraphUrl}`);
+          // Ternary because cannot dynamically access environment variables in the browser
+          const paymentsSubgraphUrl =
+            chain === "arbitrum-one"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_ARBITRUM_ONE
+              : chain === "avalanche"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_AVALANCHE
+              : chain === "base"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_BASE
+              : chain === "bsc"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_BSC
+              : chain === "celo"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_CELO
+              : chain === "core"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_CORE
+              : chain === "fantom"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_FANTOM
+              : chain === "fuse"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_FUSE
+              : chain === "mainnet"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_MAINNET
+              : chain === "matic"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_MATIC
+              : chain === "moonbeam"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_MOONBEAM
+              : chain === "near"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_NEAR
+              : chain === "near-testnet"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_NEAR_TESTNET
+              : chain === "optimism"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_OPTIMISM
+              : chain === "ronin"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_RONIN
+              : chain === "sepolia"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_SEPOLIA
+              : chain === "tombchain"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_TOMBCHAIN
+              : chain === "xdai"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_XDAI
+              : chain === "zksyncera"
+              ? process.env.NEXT_PUBLIC_PAYMENTS_SUBGRAPH_URL_ZKSYNCERA
+              : undefined;
+          if (!paymentsSubgraphUrl) {
+            throw new Error(`Unknown chain: ${chain}`);
+          }
           return getTheGraphClient(chain, paymentsSubgraphUrl);
-        }
-      }
+        },
+      },
     });
 
     setter(requestNetwork);
