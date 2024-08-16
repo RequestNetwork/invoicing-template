59dee97b263c7aeda8ea474a2e6b26b9317b6985
Aimen Sahnoun <aimensahnoun@outlook.com>
2024-08-16 11:20:59 +0300

    feat: add GTM to the templates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import "@/styles/globals.css";
-import { Montserrat } from "next/font/google";
-import type { AppProps } from "next/app";
 import Navbar from "@/components/common/Navbar";
+import "@/styles/globals.css";
 import { Provider } from "@/utils/context";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { init, Web3OnboardProvider } from "@web3-onboard/react";
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
 import { onboardConfig } from "../utils/connectWallet";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -24,6 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Provider>
       </Web3OnboardProvider>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />
     </div>
   );
 }
