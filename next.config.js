6864fc5c8e346075b1f420e22e185a8cb92dc027
rodrigopavezi <rodrigopavezi@gmail.com>
2024-12-14 05:13:17 -0300

    fix: build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbo: true,
+    // Modern options for Next.js optimization
+    optimizePackageImports: ['@lit-protocol/lit-node-client', '@requestnetwork/lit-protocol-cipher'],
+    serverActions: true,
   },
+  // Add any other necessary configuration options
 }
 
 module.exports = nextConfig 
