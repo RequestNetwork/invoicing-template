fd8b0787c9ad0975f6d10c69203517d65a61a6b4
rodrigopavezi <rodrigopavezi@gmail.com>
2024-12-14 05:31:36 -0300

    fix: revert

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['@lit-protocol/lit-node-client', '@requestnetwork/lit-protocol-cipher'],
-  },
-}
-
-module.exports = nextConfig 
